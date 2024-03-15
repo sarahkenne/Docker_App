39a978c43a1dc87f4c6fa93f20e0254340e3b961
sarahkenne <157692616+sarahkenne@users.noreply.github.com>
2024-03-15 11:20:27 +0100

    update

--- Frontend/validation.php
+++ Frontend/validation.php
@@ -4,10 +4,15 @@ $prenom = $_POST['prenom'];
 $email = $_POST['email'];
 $sujet = $_POST['demande'];
 
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+
 $bd = new PDO(
-  'mysql:host=localhost; dbname=astro; charset=utf8',
-  'root',
-  ''
+  "mysql:host=$host;dbname=$dbname;charset=utf8",
+  $username,
+  $password
 );
 $requete = 'INSERT INTO users (nom,prenom,email,sujet) VALUES (:nom, :prenom, :email, :sujet)';
 $prepare = $bd->prepare($requete);
